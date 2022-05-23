acd74bb2b8466450d6644c2e30d4f600f3da23a4
pbgm5 <Priya.Begum@mail.citytech.cuny.edu>
2022-05-22 23:32:49 -0400

    added code

--- FinalProject_Arduino_Digital_Bunny_Pet_Tamagatchi/FinalProject_Arduino_Digital_Bunny_Pet_Tamagatchi.ino
+++ FinalProject_Arduino_Digital_Bunny_Pet_Tamagatchi/FinalProject_Arduino_Digital_Bunny_Pet_Tamagatchi.ino
@@ -1,47 +1,29 @@
-byte val;
-const int LED1 = 13;
-const int LED2 = 12;
-const int LED3 = 8;
-//const int Button1 = ~5;
-//int buttonState1 = 0;
-//int button = 0;
+const int Button1 = 2;
+int buttonState1 = 0;
+const int Button2 = 4;
+int buttonState2 = 0;
+const int Button3 = 7;
+int buttonState3 = 0;
 
 void setup() {
-  pinMode(LED1, OUTPUT); 
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  //pinMode(Button1, INPUT);
+  pinMode(Button1, INPUT);
+  pinMode(Button2, INPUT);
+  pinMode(Button3, INPUT);
   Serial.begin(9600);
-
+  
 }
 
 void loop() {
- // buttonState1 = digitalRead(Button1);
-  if (Serial.available()) { 
-    val = Serial.read(); 
-  }
-  if (val == 0) { 
-    digitalWrite(LED1, LOW); 
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED3, LOW);
-  }
-  else if (val == 1) { 
-    digitalWrite(LED1, HIGH); 
-    digitalWrite(LED2, LOW); 
-    digitalWrite(LED3, LOW);
-  }
-  else if (val == 2) { 
-    digitalWrite(LED2, HIGH); 
-    digitalWrite(LED1, LOW); 
-    digitalWrite(LED3, LOW);
-  }
-  else if (val == 3) {
-    digitalWrite(LED3, HIGH);
-    digitalWrite(LED2, LOW); 
-    digitalWrite(LED1, LOW);
-  }
-  delay(10); 
-  /*if (buttonState1 == HIGH) {
-    button = 1;
-  }*/
+   buttonState1 = digitalRead(Button1);
+   buttonState2 = digitalRead(Button2);
+   buttonState3 = digitalRead(Button3);
+
+    delay(100);
+    Serial.write(buttonState1);
+    //Serial.println(buttonState1);
+    //Serial.println(buttonState2);
+    //Serial.println(buttonState3);
+    Serial.write(buttonState2);
+    Serial.write(buttonState3);
+    
 }
